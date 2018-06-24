edfd81814557906c0a5757dd4b1ec78bb94b35c2
Luke Vo <datvm@outlook.com>
2018-06-25 01:29:55 +0700

    Initialize Demo project.

---
+++ Demo/MinApp.Demo.Backend/AppCore.cs
@@ -0,0 +1,20 @@
+﻿using MinApp.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinApp.Demo.Backend
+{
+
+    public static class AppCore
+    {
+
+        public static IServer Server { get; private set; } 
+
+
+
+    }
+
+}
